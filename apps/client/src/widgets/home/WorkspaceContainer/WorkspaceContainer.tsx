a277c68a2778e1e7cf727375e6e27640d686b4c6
leeyeongjae <xingxing2001@cau.ac.kr>
2025-01-09 13:35:02 +0900

    ✨ feat: 가상스크롤 구현 중

--- apps/client/src/widgets/home/WorkspaceContainer/WorkspaceContainer.tsx
+++ apps/client/src/widgets/home/WorkspaceContainer/WorkspaceContainer.tsx
@@ -1,8 +1,26 @@
 import { EmptyWorkspace, WorkspaceGrid, WorkspaceHeader, WorkspaceList } from '@/widgets';
-import { useGetWorkspaceList, useInfiniteScroll } from '@/shared/hooks';
+import {
+  useGetWorkspaceList,
+  useInfiniteScroll,
+  useScrollPostion,
+  useWindowSize,
+} from '@/shared/hooks';
 
 import { SkeletonWorkspaceList } from '@/shared/ui';
 import { WorkspaceLoadError } from '@/entities';
+import { useEffect, useState } from 'react';
+import { TWorkspace } from '@/shared/types';
+
+const calculateNumOfNodePadding = (windowWidth: number): number => {
+  if (windowWidth < 640) {
+    return 1;
+  } else if (windowWidth < 768) {
+    return 2;
+  } else if (windowWidth < 1024) {
+    return 3;
+  }
+  return 4;
+};
 
 /**
  *
@@ -10,8 +28,52 @@ import { WorkspaceLoadError } from '@/entities';
  * 워크스페이스 헤더와 그리드를 감싸는 컨테이너 컴포넌트
  */
 export const WorkspaceContainer = () => {
+  const { scrollPosition } = useScrollPostion();
+  const { screenHeight, screenWidth } = useWindowSize();
+
+  useEffect(() => {
+    console.log(scrollPosition);
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    console.log(screenHeight, screenWidth);
+  }, [screenHeight, screenWidth]);
+
   const { hasNextPage, fetchNextPage, isPending, isFetchingNextPage, isError, workspaceList } =
     useGetWorkspaceList();
+
+  const [renderedData, setRenderedData] = useState<TWorkspace[]>([]);
+
+  const topSectionHeight = 594;
+  const workspaceItemHeight = 262;
+
+  const nodePadding = calculateNumOfNodePadding(screenWidth);
+  const gapY = 32;
+  const start = Math.max(
+    0,
+    Math.floor((scrollPosition - topSectionHeight) / (workspaceItemHeight + gapY)) * nodePadding
+  );
+  console.log(`start : ${start}`);
+
+  useEffect(() => {
+    if (!workspaceList) {
+      return;
+    }
+    console.log(nodePadding);
+
+    setRenderedData(
+      workspaceList.slice(
+        start,
+        start +
+          Math.floor(screenHeight / (workspaceItemHeight + gapY)) * nodePadding +
+          2 * nodePadding
+      )
+    );
+  }, [start, screenHeight, workspaceList]);
+
+  useEffect(() => {
+    console.log(renderedData);
+  }, [renderedData]);
 
   const fetchCallback: IntersectionObserverCallback = (entries, observer) => {
     entries.forEach((entry) => {
@@ -39,8 +101,8 @@ export const WorkspaceContainer = () => {
         (workspaceList.length === 0 ? (
           <EmptyWorkspace />
         ) : (
-          <WorkspaceGrid>
-            <WorkspaceList workspaceList={workspaceList} />
+          <WorkspaceGrid offsetY={Math.floor(start / nodePadding) * (workspaceItemHeight + gapY)}>
+            <WorkspaceList workspaceList={renderedData} />
             {isFetchingNextPage && <SkeletonWorkspaceList skeletonNum={8} />}
           </WorkspaceGrid>
         ))
--- apps/client/src/widgets/home/WorkspaceGrid/WorkspaceGrid.tsx
+++ apps/client/src/widgets/home/WorkspaceGrid/WorkspaceGrid.tsx
@@ -1,13 +1,22 @@
-import { PropsWithChildren } from 'react';
+import { ReactNode } from 'react';
 
 /**
  *
  * @description
  * 워크스페이스 그리드 컴포넌트
  */
-export const WorkspaceGrid = ({ children }: PropsWithChildren) => {
+export const WorkspaceGrid = ({
+  offsetY = 0,
+  children,
+}: {
+  offsetY?: number;
+  children: ReactNode;
+}) => {
   return (
-    <div>
+    <div
+      style={{ transform: offsetY > 0 ? `translateY(${offsetY + 32}px)` : 'none' }}
+      will-change="transform"
+    >
       <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {children}
       </div>
