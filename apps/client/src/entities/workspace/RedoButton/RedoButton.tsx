5e07f24bb8cded0a693dc0409f0c7eb303ee220d
chichoc <chichoc.dev@gmail.com>
2025-01-21 15:48:46 +0900

    🔨 refactor: 헤더 상단 버튼 각각 메모이제이션

--- apps/client/src/entities/workspace/RedoButton/RedoButton.tsx
+++ apps/client/src/entities/workspace/RedoButton/RedoButton.tsx
@@ -1,12 +1,13 @@
 import { CircleButton } from '@/shared/ui';
 import RightArrow from '@/shared/assets/arrow_right.svg?react';
 import { useWorkspaceStore } from '@/shared/store';
+import { memo } from 'react';
 
 /**
  * @description
  * 워크스페이스 캔버스에서 redo 기능을 실행시키는 버튼입니다.
  */
-export const RedoButton = () => {
+export const RedoButton = memo(() => {
   const { workspace } = useWorkspaceStore();
 
   const handleRedo = () => {
@@ -20,4 +21,4 @@ export const RedoButton = () => {
       <RightArrow />
     </CircleButton>
   );
-};
+});
