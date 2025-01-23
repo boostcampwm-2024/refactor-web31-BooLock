df789f47493f3fbb775ba8b9aea23a7f28e596f5
lee0jae330 <xingxing2001@cau.ac.kr>
2025-01-23 22:59:44 +0900

    🎨 style: 불필요한 스타일 코드 삭제

--- apps/client/src/entities/workspace/CssTooltip/CssTooltip.tsx
+++ apps/client/src/entities/workspace/CssTooltip/CssTooltip.tsx
@@ -11,7 +11,7 @@ export const CssTooltip = ({ description, leftX, topY }: CssTooltipProps) => {
   const { tooltipX, tooltipY, tooltipRef } = useCssTooltip(leftX, topY);
   return createPortal(
     <div
-      className={`text-gray-white text-tooltip-sm fixed left-0 top-0 z-[9999] rounded-3xl ${tooltipY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
+      className={`text-gray-white text-tooltip-sm fixed z-[9999] rounded-3xl ${tooltipY >= 0 ? 'rounded-tl-none' : 'rounded-bl-none'} bg-green-500 px-3 py-2`}
       style={{
         left: `${tooltipX + 18}px`,
         top: tooltipY >= 0 ? `${tooltipY + 8}px` : `${-tooltipY}px`,
