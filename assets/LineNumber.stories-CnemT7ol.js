f5dd12f461a92ac9de8787feab1618772fc9c855
lee0jae330 <lee0jae330@users.noreply.github.com>
2025-01-23 14:03:18 +0000

    deploy: 24b22e23f74c3522d2fb2032feec6c1a010425c9

--- assets/LineNumber.stories-CnemT7ol.js
+++
@@ -1,5 +0,0 @@
-import{L as a}from"./LineNumbers-B_2_ToYE.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";const c={title:"shared/code-highlighter/LineNumbers",component:a,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{codeLineList:["line1","line2","line3"]}};var r,t,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    codeLineList: ['line1', 'line2', 'line3']
-  }
-}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const m=["Default"];export{e as Default,m as __namedExportsOrder,c as default};
