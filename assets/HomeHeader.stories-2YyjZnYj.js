2cfaaeee098487150f16ac0b25d99a3f85ef9099
lee0jae330 <lee0jae330@users.noreply.github.com>
2025-01-24 06:46:27 +0000

    deploy: 8e6a0577a24ca6b285d6606cd3b64393aeeaaa27

--- assets/HomeHeader.stories-2YyjZnYj.js
+++
@@ -1,9 +0,0 @@
-import{H as i}from"./HomeHeader-DhJwOzpt.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./plus-C7enkl2d.js";import"./SquareButton-eIFPi1Hz.js";import"./Logo-BbusgMO-.js";import"./index-x70cJmE6.js";import"./index-BXCeiWJk.js";import"./ToasterWithMax-D4jPvwZi.js";import"./index-D1AAVYfs.js";import"./Loading-CVQUyNmW.js";import"./Spinner-Ddn5mi22.js";import"./mixpanel-BYnboQeE.js";import"./react-72mb-naO.js";import"./SkeletonWorkspace-D4UalYBK.js";import"./SkeletonWorkspaceList-LqBByOXs.js";import"./HoveredEmptyWorkspace-CeZW0uca.js";import"./QueryClientProvider-BdRu95_o.js";import"./ImageTagModalListItem-D30WR0c8.js";import"./WorkspaceLoadError-BuktD2D2.js";import"./NotHoveredEmptyWorkspace-loFxOJJf.js";import"./question-BUxmZXrP.js";import"./CssCategoryButton-Ctai2tKd.js";import"./client-Dbo3a7kb.js";import"./ImageTagModalHeader-CJTUWoJB.js";import"./ImageTagModalImg-BJAMLe19.js";import"./ImageTagModalButton-DJiJ58eG.js";import"./useCoachMarkStore-Bn0-Ay5q.js";const I={title:"widgets/home/HomeHeader",component:i,parameters:{layout:"fullscreen"},tags:["autodocs"]},r={args:{}},o={args:{isBlack:!0}};var t,e,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    // propsname: value,
-  }
-}`,...(m=(e=r.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};var a,p,s;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    isBlack: true
-  }
-}`,...(s=(p=o.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};const J=["Default","Dark"];export{o as Dark,r as Default,J as __namedExportsOrder,I as default};
