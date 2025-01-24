344f085c07a62dc307f5cb0c24d20a3f615b3a7b
inhachoi <inhachoi@users.noreply.github.com>
2025-01-24 02:32:33 +0000

    deploy: 28cbcf8f73330814a9f01928f966840042aaf75d

---
+++ assets/PreviewBox.stories-ucHNbayZ.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{P as s}from"./PreviewBox-Bvn_0S72.js";import"./index-DRjF_FHU.js";import"./mixpanel-BYnboQeE.js";import"./react-72mb-naO.js";import"./CodeViewer-dG1omHtV.js";import"./LineNumbers-B_2_ToYE.js";import"./useCoachMarkStore-Bn0-Ay5q.js";import"./index-D1AAVYfs.js";const g={title:"widgets/workspace/PreviewBox",component:s,parameters:{layout:"centered"},decorators:[n=>e.jsx("div",{style:{width:"500px",height:"100%"},children:e.jsx(n,{})})],tags:["autodocs"]},o={args:{htmlCode:`<html>
+      <head></head>
+      <body>
+        <div class="container">
+          <h1 class="title">Hello, world!</h1>
+        </div>
+      </body>
+    </html>`,cssCode:`.container {
+      background-color: #f0f0f0;
+      padding: 1rem;
+    }
+.title {
+      color: #43a135;
+      font-size: 2rem;
+    }
+    `}};var t,r,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    htmlCode: \`<html>
+      <head></head>
+      <body>
+        <div class="container">
+          <h1 class="title">Hello, world!</h1>
+        </div>
+      </body>
+    </html>\`,
+    cssCode: \`.container {
+      background-color: #f0f0f0;
+      padding: 1rem;
+    }
+.title {
+      color: #43a135;
+      font-size: 2rem;
+    }
+    \`
+  }
+}`,...(a=(r=o.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const x=["Default"];export{o as Default,x as __namedExportsOrder,g as default};
